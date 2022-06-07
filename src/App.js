b7bf89659b9befab49ef04acafac08a95dc36d9d
nurabdu <abdurashitov.beknur@gmail.com>
2022-06-07 22:37:59 +0300

    comment added

--- src/App.js
+++ src/App.js
@@ -5,10 +5,11 @@ import { useDispatch, useSelector } from 'react-redux';
 function App() {
   const dispatch = useDispatch();
   const cash = useSelector(state => state.cash);
-  
+  // add cash
   const addcash = (cash) => {
     dispatch({type: "ADD_CASH", payload: cash})
   };
+  //get cash
   const getcash = (cash) => {
     dispatch({type: "GET_CASH", payload: cash})
   };
